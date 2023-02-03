da421fd9ced726efc4cfc4db29afd3c4c8dba310
Abin Joseph <abin.joseph@tigeranalytics.com>
2023-02-03 12:04:51 +0530

    removing the pyspy approach

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-pyroscope exec gunicorn --bind 0.0.0.0:8000 startup:app --log-level DEBUG --workers $GUNICRON_WORKERS --threads $GUNICORN_THREADS 
+gunicorn --bind 0.0.0.0:8000 startup:app --log-level DEBUG --workers $GUNICRON_WORKERS --threads $GUNICORN_THREADS 
