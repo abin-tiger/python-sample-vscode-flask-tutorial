548391398d99d5f18c4ac35251726e3f7c64118a
Abin Joseph <abin.joseph@tigeranalytics.com>
2023-02-03 12:49:31 +0530

    trying more options

--- startup.py
+++ startup.py
@@ -15,7 +15,10 @@ import os
 pyroscope.configure(
   application_name = "my.python.app", # replace this with some name for your application
   server_address = os.environ["PYROSCOPE_SERVER_ADDRESS"],
-  detect_subprocesses = True
+  detect_subprocesses = True,
+  oncpu               = False,
+  gil_only            = True,
+
 )
 
 from hello_app.webapp import app
